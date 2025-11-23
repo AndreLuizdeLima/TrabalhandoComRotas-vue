0896bc6e4592ee6dc305c57a3733234745e6ba44
Andre <andreluizjp2015@gmail.com>
2025-11-23 18:29:07 -0300

    criando lead

---
+++ src/components/Vendas/Lead.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h5>{{dados.id}} - {{ dados.nome }}</h5>
+
+        <div class="mb-3 row">
+            <label for="col-sm-2 col-form-label">Nome</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" :value="dados.nome">
+            </div>
+        </div>
+
+        <div class="mb-3 row">
+            <label for="col-sm-2 col-form-label">Telefone</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" :value="dados.telefone">
+            </div>
+        </div>
+
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary">Atualizar</button>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+import ApiMixin from '@/mixins/ApiMixin';
+
+export default {
+    name: 'LeadVendas',
+    mixins: [ApiMixin],
+    data: () => ({
+    }),
+    methods: {
+    },
+    created() {
+        this.getDadosApi(`http://localhost:3000/leads/${this.$route.params.id}`)
+    }
+}
+</script>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import ServicosHome from './components/serviços/Servicos.vue'
 import LeadsVendas from './components/vendas/Leads.vue'
 import ContratosVendas from './components/vendas/Contratos.vue'
 import DashboardHome from './components/dashboard/Dashboard.vue'
+import LeadVendas from './components/vendas/Lead.vue'
 
 const routes = [
     {
@@ -18,6 +19,7 @@ const routes = [
                 component: VendasHome,
                 children: [
                     {path: 'leads', component: LeadsVendas },
+                    {path: 'leads/:id', component: LeadVendas },
                     {path: 'contratos', component: ContratosVendas },
                 ]
             },
