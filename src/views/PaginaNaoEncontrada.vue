b55ad80c8bd2ab54baf8b4420c18b951b2d51fb1
Andre <andreluizjp2015@gmail.com>
2025-12-02 20:24:53 -0300

    criando pagina not found

--- src/views/PaginaNaoEncontrada.vue
+++ src/views/PaginaNaoEncontrada.vue
@@ -1,13 +1,27 @@
 <template>
-  <h5>-_- Opsss. Essa Página não foi encontrada!</h5>
+  <div class="container">
+    <div class="card">
+      <h2 class="card-title">404 Error...</h2>
+      <h5 class="card-title">-_- Opsss. Essa Página não foi encontrada!</h5>
+      <br>
+      <router-link class="btn btn-primary" to="/">Voltar</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'PaginaNaoEncontrada'
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  padding-top: 40vh;
+}
 
+.card{
+  padding: 15px;
+  align-items: center;
+}
 </style>
